47361774b73c5fc3528f5f003201fd6c49ccd81e
Chua Wei Jie <cweijie@cmkl.ac.th>
2025-03-06 14:34:40 +0700

    Fixed file typo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mv -f .TheNewton_WP.JPEG ~/.TheNewton_WP.JPEG
-mv -f .startup_cleanup.sh ~/.startup_cheanup.sh
+mv -f .startup_cleanup.sh ~/.startup_cleanup.sh
 chmod +x ~/.startup_cleanup.sh
 nano com.user.startupcleanup.plist
 mv -f com.user.startupcleanup.plist ~/Library/LaunchAgents/com.user.startupcleanup.plist
