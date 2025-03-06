33c49f28111ad728cac1945451ef4a93272b72e4
Chua Wei Jie <cweijie@cmkl.ac.th>
2025-03-06 14:32:32 +0700

    Added a line to launch text editor

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mv -f .TheNewton_WP.JPEG ~/.TheNewton_WP.JPEG
 mv -f .startup_cleanup.sh ~/.startup_cheanup.sh
 chmod +x ~/.startup_cleanup.sh
+nano com.user.startupcleanup.plist
 mv -f com.user.startupcleanup.plist ~/Library/LaunchAgents/com.user.startupcleanup.plist
 launchctl load ~/Library/LaunchAgents/com.user.startupcleanup.plist
