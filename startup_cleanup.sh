3f9dbd2dfd95757ce08d7af257e11ef6560f22ab
Chua Wei Jie <cweijie@cmkl.ac.th>
2025-03-06 13:18:29 +0700

    Hiding the shell file

--- startup_cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/Desktop/*
-rm -rf ~/Downloads/*
-rm -rf ~/Documents/*
-rm -rf ~/Movies/*
-
-osascript -e 'tell application "System Events" to set picture of every desktop to "/System/Library/Desktop Pictures/Hello Metallic Purple.heic"'
