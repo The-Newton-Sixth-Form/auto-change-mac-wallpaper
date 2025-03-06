93f0bb04e1e2d44de674540db9c95f058196623b
Chua Wei Jie <cweijie@cmkl.ac.th>
2025-03-06 14:16:51 +0700

    Added the install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+mv -f TheNewton_WP.JPEG /System/Library/Desktop Pictures/TheNewton_WP.JPEG
+mv -f .startup_cleanup.sh ~/.startup_cheanup.sh
+chmod +x ~/.startup_cleanup.sh
+mv -f com.user.startupcleanup.plist ~/Library/LaunchAgents/com.user.startupcleanup.plist
+launchctl load ~/Library/LaunchAgents/com.user.startupcleanup.plist
